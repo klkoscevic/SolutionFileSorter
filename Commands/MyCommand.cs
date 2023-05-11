5a75a38d7de70e02c38cd0da42c483294d353353
klarakoscevic <klkoscevic@vsite.hr>
2023-05-12 01:19:21 +0200

    Add project files.

---
+++ Commands/MyCommand.cs
@@ -0,0 +1,107 @@
+﻿using EnvDTE;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace OrderProjectsInSlnFile
+{
+    [Command(PackageIds.MyCommand)]
+    internal sealed class MyCommand : BaseCommand<MyCommand>
+    {
+        class ProjectLine
+        {
+            public string Line { get; set; }
+            public string Name { get; set; }
+        }
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            OrderProjects();
+        }
+
+        public void OrderProjects()
+        {
+            string solutionFilePath = GetSolutionPath();
+
+            if (solutionFilePath == "" || !File.Exists(solutionFilePath))
+            {
+                Debug.WriteLine($"Solution file '{solutionFilePath}' does not exist.");
+                return;
+            }
+
+            try
+            {
+                List<ProjectLine> projectLines = new List<ProjectLine>();
+                List<int> lineNumber = new List<int>();
+                string[] linesInFile = File.ReadAllLines(solutionFilePath);
+                string patternProject = @"^Project\(""\{[A-Z0-9-]+\}""\) = ""([^""]+)"",.+(}"")$";
+                string patternGlobal = @"^Global";
+
+                for (int i = 0; i < linesInFile.Length; i++)
+                {
+                    var matchesGlobal = Regex.Matches(linesInFile[i], patternGlobal);
+                    if (matchesGlobal.Count > 0)
+                    {
+                        break;
+                    }
+
+                    var matchesProject = Regex.Matches(linesInFile[i], patternProject);
+                    if (matchesProject.Count > 0)
+                    {
+                        lineNumber.Add(i);
+                        foreach (Match match in matchesProject)
+                        {
+                            string line = match.Value;
+                            string projectName = match.Groups[1].Value;
+                            projectLines.Add(new ProjectLine { Line = line, Name = projectName });
+                        }
+                    }
+                }
+
+                projectLines = projectLines.OrderBy(p => p.Name).ToList();
+
+                for (int i = 0; i < linesInFile.Length; i++)
+                {
+                    if (i == lineNumber[0])
+                    {
+                        linesInFile[i] = projectLines[0].Line;
+                        projectLines.RemoveAt(0);
+                        lineNumber.RemoveAt(0);
+
+                        if (lineNumber.Count == 0)
+                        {
+                            break;
+                        }
+                    }
+                }
+
+                File.WriteAllLines(solutionFilePath, linesInFile);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static string GetSolutionPath()
+        {
+            string currentDirectory = Environment.CurrentDirectory;
+            string solutionPath = null;
+
+            while (currentDirectory != null)
+            {
+                string[] solutionFiles = System.IO.Directory.GetFiles(currentDirectory, "*.sln");
+                if (solutionFiles.Length > 0)
+                {
+                    solutionPath = solutionFiles[0];
+                    break;
+                }
+
+                currentDirectory = System.IO.Directory.GetParent(currentDirectory)?.FullName;
+            }
+
+            return solutionPath;
+        }
+    }
+}
---
+++ OrderProjectsInSlnFilePackage.cs
@@ -0,0 +1,22 @@
+﻿global using Community.VisualStudio.Toolkit;
+global using Microsoft.VisualStudio.Shell;
+global using System;
+global using Task = System.Threading.Tasks.Task;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace OrderProjectsInSlnFile
+{
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration(Vsix.Name, Vsix.Description, Vsix.Version)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(PackageGuids.OrderProjectsInSlnFileString)]
+    public sealed class OrderProjectsInSlnFilePackage : ToolkitPackage
+    {
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await this.RegisterCommandsAsync();
+        }
+    }
+}
