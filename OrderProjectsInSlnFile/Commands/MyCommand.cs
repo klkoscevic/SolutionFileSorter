64218512f33b2a9ccc1ab4dd83a859a2282dbfd2
Julijan Šribar <julijan.sribar@gmail.com>
2023-05-30 14:28:49 +0200

    Extract solution file sorting into a separate class to make testing it easier

--- OrderProjectsInSlnFile/Commands/MyCommand.cs
+++ OrderProjectsInSlnFile/Commands/MyCommand.cs
@@ -62,63 +62,15 @@ namespace OrderProjectsInSlnFile
 
             try
             {
-                ProjectLines projectLines = new ProjectLines();
-                ProjectLines guidLines = new ProjectLines();
-
-                var linesInFile = new List<string>();
-                System.Text.Encoding encoding;
+                SolutionFile solutionFile = null;
+                System.Text.Encoding encoding = null;
                 using (var reader = new StreamReader(solutionFilePath))
                 {
-                    string line = null;
-                    while ((line = reader.ReadLine()) != null)
-                    {
-                        linesInFile.Add(line);
-                    }
+                    solutionFile = new SolutionFile(reader);
                     encoding = reader.CurrentEncoding;
                 }
 
-                string patternProject = @"^Project\(""\{[A-Z0-9-]+\}""\) = ""([^""]+)"",.+\{([A-Z0-9-]+)\}""$";
-                string patternGuid = @"^\s+\{([A-Z0-9-]+).+";
-
-                for (int i = 0; i < linesInFile.Count(); i++)
-                {
-                    var matchesProject = Regex.Matches(linesInFile[i], patternProject);
-                    var matchesGuid = Regex.Matches(linesInFile[i], patternGuid);
-
-                    if (matchesProject.Count > 0)
-                    {
-                        foreach (Match match in matchesProject)
-                        {
-                            string line = match.Value;
-                            string projectName = match.Groups[1].Value;
-                            string guid = match.Groups[2].Value;
-
-                            ProjectLine projectLine = new ProjectLine { Line = line, Name = projectName, GUID = guid };
-
-                            projectLines.Enqueue(i, projectLine);
-                        }
-                    }
-                    else if (matchesGuid.Count > 0)
-                    {
-                        foreach (Match match in matchesGuid)
-                        {
-                            string line = match.Value;
-                            string guid = match.Groups[1].Value;
-
-                            ProjectLine projectLine = new ProjectLine { Line = line, GUID = guid };
-
-                            guidLines.Enqueue(i, projectLine);
-                        }
-                    }
-                }
-
-                projectLines.Sort();
-
-                guidLines.AddProjectName(projectLines);
-                guidLines.Sort();
-
-                linesInFile = ChangeLines(projectLines, linesInFile);
-                linesInFile = ChangeLines(guidLines, linesInFile);
+                var linesInFile = solutionFile.Sort();
 
                 using (var writer = new StreamWriter(solutionFilePath, false, encoding))
                 {
@@ -145,28 +97,6 @@ namespace OrderProjectsInSlnFile
             {
                 System.Windows.Forms.MessageBox.Show(ex.Message, "An error occurred", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
-        }
-        private List<string> ChangeLines(ProjectLines projectLines, List<string> linesInFile)
-        {
-            Tuple<int, string> projectLinesDequeue = projectLines.Dequeue();
-            int nums = 0;
-            while (projectLinesDequeue != null)
-            {
-                for (int i = nums; i < linesInFile.Count; i++)
-                {
-                    nums++;
-
-                    if (i == projectLinesDequeue.Item1)
-                    {
-                        linesInFile[i] = projectLinesDequeue.Item2;
-                        break;
-                    }
-                }
-
-                projectLinesDequeue = projectLines.Dequeue();
-            }
-
-            return linesInFile;
         }
     }
 }
