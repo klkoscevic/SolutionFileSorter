04bc455c32bcec3883f18a478218b814bd3e3652
Julijan Šribar <julijan.sribar@gmail.com>
2023-05-30 15:26:32 +0200

    Add unit test project

---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("31f42745-3727-4345-826c-081033bc30ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTests/SolutionFileTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderProjectsInSlnFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class SolutionFileTests
+    {
+        [TestMethod]
+        public void SortDoesntChangeContentForEmptySolution()
+        {
+            const string sln = "\r\nMicrosoft Visual Studio Solution File, Format Version 12.00\r\n# Visual Studio Version 17\r\nVisualStudioVersion = 17.6.33723.286\r\nMinimumVisualStudioVersion = 10.0.40219.1\r\nGlobal\r\n\tGlobalSection(SolutionProperties) = preSolution\r\n\t\tHideSolutionNode = FALSE\r\n\tEndGlobalSection\r\n\tGlobalSection(ExtensibilityGlobals) = postSolution\r\n\t\tSolutionGuid = {26D0A5FF-D281-4407-9DF4-2189B568BDC9}\r\n\tEndGlobalSection\r\nEndGlobal";
+
+            SolutionFile slnFile = null;
+            using (var reader = new StringReader(sln))
+            {
+                slnFile = new SolutionFile(reader);
+            }
+            var lines = slnFile.Sort();
+
+            IEnumerable<string> original = sln.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+            Assert.IsTrue(lines.SequenceEqual(original));
+        }
+
+        [TestMethod]
+        public void SortDoesntChangeContentForSolutionWithASingleProject()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SortDoesntChangeContentForSolutionWithTwoProjectsThatAreSortedAlready()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SortReordersContentForSolutionWithTwoProjectsThatAreNotSorted()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod]
+        public void SortReordersContentForSolutionWithSeverlaProjectsAndFoldersThatAreNotSorted()
+        {
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream("UnitTests.Resources.SolutionWithFilesAndFolders.original");
+            SolutionFile slnFile = null;
+            using (var reader = new StreamReader(stream))
+            {
+                slnFile = new SolutionFile(reader);
+            }
+            var lines = slnFile.Sort();
+
+            var expected = ReadLinesFromResource("UnitTests.Resources.SolutionWithFilesAndFolders.sorted");
+
+            Assert.IsTrue(lines.SequenceEqual(expected));
+        }
+
+        private IEnumerable<string> ReadLinesFromResource(string resource)
+        {
+            var expected = new List<string>();
+            var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
+            using (var reader = new StreamReader(stream))
+            {
+                string line = null;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    expected.Add(line);
+                }
+            }
+            return expected;
+        }
+    }
+}
