9dd3a146f72eb5378677fd006140ed004a89ead7
klarakoscevic <klkoscevic@vsite.hr>
2023-06-03 12:27:53 +0200

    Added tests for Helper class

---
+++ UnitTests/HelperUnitTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderProjectsInSlnFile;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class HelperUnitTests
+    {
+        [TestMethod]
+        public void SolutionWithASingleProjectIsSorted()
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            basePath = new DirectoryInfo(basePath).Parent.Parent.FullName;
+            string solutionFilePath = Path.Combine(basePath, @"Resources\SolutionWithASingleProject.original");
+
+            Helper helper = new Helper();
+
+            var isSorted = helper.IsSlnFileSorted(solutionFilePath);
+
+            Assert.IsTrue(isSorted);
+        }
+
+        [TestMethod]
+        public void SolutionWithTwoProjectsIsSorted()
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            basePath = new DirectoryInfo(basePath).Parent.Parent.FullName;
+            string solutionFilePath = Path.Combine(basePath, @"Resources\SolutionWithTwoProjectsThatAreSortedAlready.original");
+
+            Helper helper = new Helper();
+
+            var isSorted = helper.IsSlnFileSorted(solutionFilePath);
+
+            Assert.IsTrue(isSorted);
+        }
+
+        [TestMethod]
+        public void SolutionWithTwoProjectsIsNotSorted()
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            basePath = new DirectoryInfo(basePath).Parent.Parent.FullName;
+            string solutionFilePath = Path.Combine(basePath, @"Resources\SolutionWithTwoProjectsThatAreNotSorted.original");
+
+            Helper helper = new Helper();
+
+            var isSorted = helper.IsSlnFileSorted(solutionFilePath);
+
+            Assert.IsFalse(isSorted);
+        }
+
+        [TestMethod]
+        public void SolutionWithSeveralProjectsAndFoldersIsNotSorted()
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            basePath = new DirectoryInfo(basePath).Parent.Parent.FullName;
+            string solutionFilePath = Path.Combine(basePath, @"Resources\SolutionWithFilesAndFolders.original");
+
+            Helper helper = new Helper();
+
+            var isSorted = helper.IsSlnFileSorted(solutionFilePath);
+
+            Assert.IsFalse(isSorted);
+        }
+
+        [TestMethod]
+        public void SolutionWithSeveralProjectsAndFoldersIsSorted()
+        {
+            string basePath = Directory.GetCurrentDirectory();
+            basePath = new DirectoryInfo(basePath).Parent.Parent.FullName;
+            string solutionFilePath = Path.Combine(basePath, @"Resources\SolutionWithFilesAndFolders.sorted");
+
+            Helper helper = new Helper();
+
+            var isSorted = helper.IsSlnFileSorted(solutionFilePath);
+
+            Assert.IsTrue(isSorted);
+        }
+
+        //private IEnumerable<string> ReadLinesFromResource(string resource)
+        //{
+        //    var expected = new List<string>();
+        //    var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
+        //    using (var reader = new StreamReader(stream))
+        //    {
+        //        string line = null;
+        //        while ((line = reader.ReadLine()) != null)
+        //        {
+        //            expected.Add(line);
+        //        }
+        //    }
+        //    return expected;
+        //}
+    }
+}
