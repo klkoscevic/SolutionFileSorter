4015820575ab07de8f25e2bb418c4f65f5ea4a46
Julijan Šribar <julijan.sribar@gmail.com>
2023-06-05 21:22:12 +0200

    Add ProjectsSorter class

---
+++ OrderProjectsInSlnFile/Classes/ProjectsSorter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OrderProjectsInSlnFile
+{
+    public static class ProjectsSorter
+    {
+        private static ProjectEntryComparer comparer = new ProjectEntryComparer();
+
+        public static IEnumerable<ProjectEntry> GetSorted(IEnumerable<ProjectEntry> projects)
+        {
+            return projects.OrderBy(p => p, comparer);
+        }
+
+        public static bool IsSorted(IEnumerable<ProjectEntry> projects)
+        {
+            if (projects.Count() < 2)
+            {
+                return true;
+            }
+            return !projects.Zip(projects.Skip(1), (a, b) => comparer.Compare(a, b) <= 0).Contains(false);
+        }
+    }
+}
