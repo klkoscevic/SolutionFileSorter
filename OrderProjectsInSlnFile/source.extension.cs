ba3049031cf7ab79517e356917a0cdad54d8fbfe
klarakoscevic <klkoscevic@vsite.hr>
2023-07-04 22:18:24 +0200

    Edit description for extension

--- OrderProjectsInSlnFile/source.extension.cs
+++ OrderProjectsInSlnFile/source.extension.cs
@@ -9,7 +9,7 @@ namespace OrderProjectsInSlnFile
     {
         public const string Id = "OrderProjectsInSlnFile.174ef47e-feac-4a26-9e71-7566c9439c82";
         public const string Name = "OrderProjectsInSlnFile";
-        public const string Description = @"Clicking the button will arrange the projects in the .sln file alphabetically.";
+        public const string Description = @"OrderProjectsInSlnFile is a Visual Studio extension that provides the functionality to alphabetically sort projects within an open .sln (solution) file. This extension offers three methods of sorting projects: via a button within the Visual Studio solution, upon closing the solution, and through a console application for the given solution.";
         public const string Language = "en-US";
         public const string Version = "1.0";
         public const string Author = "Company";
